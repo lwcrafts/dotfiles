14ff15f982f39f937e8917df41fd36cf8be2c69a
lw <lingwang.dev@gmail.com>
2026-09-07 11:34:49 +0800

    feat: add mason-lspconfig

--- .config/nvim/plugin/mason.lua
+++ .config/nvim/plugin/mason.lua
@@ -1,13 +1,20 @@
 require("vim-pack").add({
 	{
 		src = "williamboman/mason.nvim",
+	},
+	{
+		src = "williamboman/mason-lspconfig.nvim",
 		opts = {
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
+			automatic_installation = true,
+			ensure_installed = {
+				-- LSPs
+				"lua_ls",
+				"ts_ls",
+				"tailwindcss",
+				"pyright",
+				-- Formatters
+				-- "stylua",
+				-- "swiftformat",
 			},
 		},
 	},
